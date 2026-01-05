9c86cf4c8ac0a09e3aaaaaef42544f29e0ea04a0
Pankaj Kumar <127238811+pankaj960-crypto@users.noreply.github.com>
2026-01-06 00:21:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import random
+from turtle import Turtle,Screen
+
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="make your bit", prompt="Which one will win the game:")
+colors = ["red","orange","yellow","black","blue","pink"]
+direction = [-70,-40,-10,20,50,80]
+all_turtles = []
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=direction[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"you've won The {winning_color} turtle is the winner")
+            else:
+                print(f"you've lost The {winning_color} turtle is winner")
+
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
+
+
+
+
+
+
+screen.exitonclick()
+
+
